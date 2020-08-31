0db52c88c3317328984387e1b84d7f06e47fa3f3
Mattias Lind von Vogelsang <mattelvv@gmail.com>
2020-08-31 23:14:29 +0200

    added code for SearchBar testing, functions, store and component template

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,11 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import searchbar from './SearchBar/index.js';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  modules: {
+    searchbar: searchbar
+  }
 });
--- src/views/Shopping.vue
+++ src/views/Shopping.vue
@@ -1,13 +1,17 @@
-!<template>
-  
+<template>
+  <div>
+    <SearchBar />
+  </div>
 </template>
 
 <script>
+import SearchBar from '@/components/SearchBar/SearchBar.vue';
 export default {
-
-}
+  name: 'Shopping',
+  components: {
+    SearchBar
+  }
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
