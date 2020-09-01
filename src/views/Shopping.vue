bb9f3c37685e1b2c5c7390ffa7082b550c7a056f
Mattias Lind von Vogelsang <mattelvv@gmail.com>
2020-09-01 16:22:26 +0200

    trying to resolve merge conflicts

--- src/views/Shopping.vue
+++ src/views/Shopping.vue
@@ -1,20 +1,17 @@
 <template>
   <div>
     <SearchBar />
-    <ProductList />
     <Footer />
   </div>
 </template>
 
 <script>
-import ProductList from '@/components/ProductList/ProductList.vue';
 import SearchBar from '@/components/SearchBar/SearchBar.vue';
 import Footer from '@/components/Footer/Footer.vue';
 export default {
   name: 'Shopping',
   components: {
     SearchBar,
-    ProductList,
     Footer
   }
 };
