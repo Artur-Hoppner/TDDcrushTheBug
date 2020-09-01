a34ed270d62d99dda1e074eb38e056d065fa0948
Mattias Lind von Vogelsang <mattelvv@gmail.com>
2020-09-01 16:17:29 +0200

    resolving code so merge with dev is possible

--- src/views/Shopping.vue
+++ src/views/Shopping.vue
@@ -2,17 +2,20 @@
   <div>
     <SearchBar />
     <ProductList />
+    <Footer />
   </div>
 </template>
 
 <script>
 import ProductList from '@/components/ProductList/ProductList.vue';
 import SearchBar from '@/components/SearchBar/SearchBar.vue';
+import Footer from '@/components/Footer/Footer.vue';
 export default {
   name: 'Shopping',
   components: {
     SearchBar,
-    ProductList
+    ProductList,
+    Footer
   }
 };
 </script>
