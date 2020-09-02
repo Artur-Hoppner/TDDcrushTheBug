7b9ddc76033942f09c60392580818cf71bbf4b6f
Artur <Artur.Hoppner@hotmail.com>
2020-09-02 10:14:22 +0200

    added test code

--- src/components/CartIcon/CartIcon.vue
+++ src/components/CartIcon/CartIcon.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <router-link to="/cart">cartIcon</router-link>
+    <!-- change link to hamburge menu later. This is for controlling that the test works -->
   </div>
 </template>
 
--- src/components/CartIcon/__tests__/test.js
+++ src/components/CartIcon/__tests__/test.js
@@ -1,41 +1,50 @@
 import {
   shallowMount,
   mount,
-  createLocalVue,
-  RouterLinkStub
+  RouterLinkStub,
+  createLocalVue
 } from '@vue/test-utils';
 import Vuex from 'vuex';
 import CartIcon from '@/components/CartIcon/CartIcon.vue';
 
-// const localVue = createLocalVue();
-// localVue.use(VueRouter);
-// const $route = {
-//   path: '/shopingcart'
-// };
+const localVue = createLocalVue();
+localVue.use(Vuex);
 
 describe('User can see numbers of selected cart items and on click go to cart', () => {
-  // test('test if in numbers of cartitems is displayed', () => {
-  //   //Act
-  //   //Assert
-  //   //Assemble
-  //   expect(true).toBe(true);
-  // });
+  const cartItems = {
+    id: '01',
+    tag: 'bugspray',
+    title: 'Hiroshima Bugspray',
+    desc: 'It will nuke your bugs!',
+    price: '120'
+  };
 
-  // test('test if link to cartlist works', () => {
-  //   //Act
-  //   const wrapper = shallowMount(CartIcon, localVue);
-  //   //Assert
-  //   //Assemble
+  test('test if link to cartlist works', () => {
+    //Act
+    const wrapper = shallowMount(CartIcon, {
+      localVue,
+      propsData: {
+        cartItems
+      }
+    });
+    console.log(cartItems.length, 'test length of product should be 1');
+    const iconContent = wrapper.text();
+    //Assert
+    // send item to cart
+    //Assemble
 
-  //   expect(true).toBe(true);
-  // });
+    expect(iconContent).toBe('cartIcon');
+    expect(cartState.state.cart.length).toBeGreaterThan(0);
+  });
 
   test('test of router-link to /cart exists?', () => {
+    //Act
     const wrapper = mount(CartIcon, {
       stubs: {
         RouterLink: RouterLinkStub
       }
     });
+    //Assert & Assemble
     expect(wrapper.findComponent(RouterLinkStub).props().to).toBe('/cart');
   });
 });
