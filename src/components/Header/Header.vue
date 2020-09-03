15ffc0e6f866c0b141c64870df8d0920d89a0401
Mattias Lind von Vogelsang <mattelvv@gmail.com>
2020-09-03 12:11:37 +0200

    trying again.

---
+++ src/components/Header/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <CartIcon />
+</template>
+
+<script>
+import CartIcon from '@/components/CartIcon/CartIcon.vue';
+
+export default {
+  components: {
+    CartIcon
+  }
+};
+</script>
+
+
+<style>
+</style>
