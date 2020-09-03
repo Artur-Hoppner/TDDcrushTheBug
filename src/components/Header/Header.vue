d593294ab9b5249c7f46a5bf0a6fc644274cdfc6
Mattias Lind von Vogelsang <mattelvv@gmail.com>
2020-09-03 11:57:18 +0200

    trying to resolve conflicts with dev

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -1,16 +1,19 @@
 <template>
   <nav class="header">
+    <CartIcon />
     <HamburgerIcon />
   </nav>
 </template>
 
 <script>
+import CartIcon from '@/components/CartIcon/CartIcon.vue';
 import HamburgerIcon from '@/components/HamburgerIcon/HamburgerIcon.vue';
 //TODO: import cartIcon when it's finished
 
 export default {
   name: 'Header',
   components: {
+    CartIcon,
     HamburgerIcon
   }
 };
