d8b5a3d87bd0007fc1953663607a3f9587cec200
Artur <Artur.Hoppner@hotmail.com>
2020-09-03 11:36:40 +0200

    imported cartIcon to Header

--- src/views/Shopping.vue
+++ src/views/Shopping.vue
@@ -4,21 +4,18 @@
     <SearchBar />
     <ProductList />
     <Footer />
-    <CartIcon />
   </div>
 </template>
 
 <script>
 import SearchBar from '@/components/SearchBar/SearchBar.vue';
 import Footer from '@/components/Footer/Footer.vue';
-import CartIcon from '@/components/CartIcon/CartIcon.vue';
 import ProductList from '@/components/ProductList/ProductList.vue';
 export default {
   name: 'Shopping',
   components: {
     SearchBar,
     Footer,
-    CartIcon,
     ProductList
   }
 };
