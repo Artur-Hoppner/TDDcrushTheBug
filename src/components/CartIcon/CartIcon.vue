01abea1ab0ac2652e7f7044229d0cb4a28bb87af
Artur <Artur.Hoppner@hotmail.com>
2020-09-04 11:04:26 +0200

    conflict with save

--- src/components/CartIcon/CartIcon.vue
+++ src/components/CartIcon/CartIcon.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <router-link  id="presentedCartItems" to="/cart">
-      <img src="@/assets/shopping-cart.svg" alt="">
+    <router-link id="presentedCartItems" to="/cart">
+      <img src="@/assets/shopping-cart.svg" alt />
     </router-link>
     <p>{{cartItem.length}}</p>
   </div>
