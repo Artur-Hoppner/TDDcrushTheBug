4041a0deaaf81e761162abb340cc9da53dc1a8f4
Artur <Artur.Hoppner@hotmail.com>
2020-09-07 09:38:00 +0200

    added preliminary grid to app

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <Header class="header" />
 
-    <router-view />
+    <router-view class="main" />
     <Footer class="footer" />
   </div>
 </template>
@@ -27,13 +27,25 @@ body #app {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
+  display: grid;
+  grid-template-rows: 50vh auto 10vh;
+  .header {
+    grid-row: 1;
+    grid-row-end: 2;
+  }
 
-.footer {
-  background: #a5d1bc;
-  // position: absolute;
-  left: 0;
-  right: 0;
-  bottom: 0;
+  .main {
+    grid-row-start: 2;
+    grid-row-end: 3;
+  }
+  .footer {
+    grid-row-start: 3;
+    grid-row-end: 4;
+    background: #a5d1bc;
+    // position: absolute;
+    left: 0;
+    right: 0;
+    bottom: 0;
+  }
 }
 </style>
--- src/views/Landing/Landing.vue
+++ src/views/Landing/Landing.vue
@@ -1,20 +1,16 @@
 <template>
   <div>
-    <h3>
-      Repell, Remove or Blast your bugs. <br />
-      We have the solution for all your pest problems
-    </h3>
-    <img class="ant1" src="@/assets/backgroundImages/ant-1.png" alt="ant 1" />
-    <h3>
-      We are a small family business that have <br />
-      a long tradition hating insects!
-    </h3>
-    <img
-      class="exterminator"
-      src="@/assets/backgroundImages/exterminator-1.png"
-      alt="exterminator"
-    />
-    <img class="ant2" src="@/assets/backgroundImages/ant-2.png" alt="ant 2" />
+    <img class="ant1" src="@/assets/backgroundImages/ant-3.png" alt="ant 1" />
+    <p>
+      Repell, Remove or Blast your bugs.
+      <br />We have the solution for all your pest problems
+    </p>
+    <p>
+      We are a small family business that have
+      <br />a long tradition hating insects!
+    </p>
+    <img class="exterminator" src="@/assets/backgroundImages/exterminator-5.png" alt="exterminator" />
+    <img class="ant2" src="@/assets/backgroundImages/ant-1.png" alt="ant 2" />
   </div>
 </template>
 
@@ -25,4 +21,15 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.ant1 {
+  width: 10vw;
+  float: left;
+}
+.exterminator {
+  width: 15vw;
+}
+.ant2 {
+  width: 15vw;
+}
+</style>
