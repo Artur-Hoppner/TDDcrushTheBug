610c787e11875cfe5a607b30e282a10d39e2d423
Mattias Lind von Vogelsang <mattelvv@gmail.com>
2020-09-07 18:37:16 +0200

    typo fix in landing page, snapshot update

--- src/views/Landing/Landing.vue
+++ src/views/Landing/Landing.vue
@@ -12,9 +12,9 @@
             class="link-to-shop"
             id="link-to-shop"
             href
-            @click="goTo('/')"
-          >Check out our product</a>
-          , and become a professianal exterminatori yourself
+            @click="goTo('/shopping')"
+            >Check out our products</a
+          >, and become a professional exterminator yourself
           <br />
           <br />We are a small family business that have a long tradition of
           hating insects!
