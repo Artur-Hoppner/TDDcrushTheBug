a284e451a63270f3f0d454fed7b94584e13f5c90
Mattias Lind von Vogelsang <mattelvv@gmail.com>
2020-09-07 12:58:47 +0200

    added styling for increment, decrement, addtocart, removefromcart in ProductItem

--- src/components/CartList/CartList.vue
+++ src/components/CartList/CartList.vue
@@ -55,7 +55,8 @@ export default {
     #2193b0
   ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
   color: #ffffff;
-  margin: 50px;
+  margin-bottom: 50px;
+  margin-top: 20px;
   cursor: pointer;
   font-size: 2em;
   padding: 1.5rem;
--- src/components/ProductItem/ProductItem.vue
+++ src/components/ProductItem/ProductItem.vue
@@ -12,26 +12,42 @@
       <p>{{ product.desc }}</p>
     </div>
     <section v-if="getProductButtonToggle">
-      <aside class="increaseQuantity">
-        <h1 @click="increaseThisQuantity(product)">+</h1>
-      </aside>
       <div>
         <h2>Quantity: {{ product.quantity }}</h2>
       </div>
-      <aside class="decreaseQuantity">
-        <h1 @click="decreaseThisQuantity(product)">-</h1>
-      </aside>
+      <div class="text-center">
+        <button
+          class="icon-btn add-btn green"
+          @click="increaseThisQuantity(product)"
+        >
+          <div class="add-icon"></div>
+          <div class="btn-txt">Add</div>
+        </button>
+        <button class="icon-btn add-btn" @click="decreaseThisQuantity(product)">
+          <div class="btn-txt">Remove</div>
+        </button>
+      </div>
     </section>
     <div>
       <h3>{{ product.price }}Kr</h3>
     </div>
-    <aside v-if="!getProductButtonToggle" class="addProduct" @click="addThisToCart(product)">
-      <!--TODO: add image to the click add product functionality <img /> -->
-      <h1>ADD</h1>
+    <aside
+      v-if="!getProductButtonToggle"
+      class="addProduct"
+      @click="addThisToCart(product)"
+    >
+      <img id="addToCart" src="@/assets/buy.png" alt="add to cart" />
     </aside>
-    <aside v-if="getProductButtonToggle" class="removeProduct" @click="removeThisFromCart(product)">
-      <!--TODO: add image to the click add product functionality <img /> -->
-      <h1>REMOVE</h1>
+    <aside
+      v-if="getProductButtonToggle"
+      class="removeProduct"
+      @click="removeThisFromCart(product.id)"
+    >
+      <img
+        id="removeFromCart"
+        src="@/assets/trash.png"
+        alt="remove from cart"
+      />
     </aside>
   </section>
 </template>
@@ -60,8 +76,134 @@ export default {
   }
 };
 </script>
-<style>
+<style scoped>
+@import url('https://fonts.googleapis.com/css?family=Montserrat&display=swap');
+
+h2,
+h3 {
+  font-family: 'Montserrat', sans-serif;
+}
+
+#removeFromCart,
+#addToCart {
+  width: 3vw;
+  height: 3vw;
+  transition: all 0.2s ease-in-out;
+}
+
+#removeFromCart:hover,
+#addToCart:hover {
+  cursor: pointer;
+  transform: scale(1.1);
+}
+
+#removeFromCart:active,
+#addToCart:active {
+  transform: scale(1.6);
+}
+
+.product {
+  margin: 40px;
+}
+
+.text-center {
+  text-align: center;
+  margin-top: 30px;
+}
+
+.icon-btn {
+  width: 50px;
+  height: 50px;
+  border: 1px solid #cdcdcd;
+  background: white;
+  border-radius: 25px;
+  overflow: hidden;
+  position: relative;
+  transition: width 0.2s ease-in-out;
+}
+.add-btn:hover {
+  width: 120px;
+}
+.add-btn::before,
+.add-btn::after {
+  transition: width 0.2s ease-in-out, border-radius 0.2s ease-in-out;
+  content: '';
+  position: absolute;
+  height: 4px;
+  width: 10px;
+  top: calc(50% - 2px);
+  background: red;
+}
+.green::before,
+.green::after {
+  top: calc(50% - 2px);
+  background: green;
+}
+
+.add-btn::after {
+  right: 14px;
+  overflow: hidden;
+  border-top-right-radius: 2px;
+  border-bottom-right-radius: 2px;
+}
+.add-btn::before {
+  left: 14px;
+  border-top-left-radius: 2px;
+  border-bottom-left-radius: 2px;
+}
+.icon-btn:focus {
+  outline: none;
+}
+.btn-txt {
+  opacity: 0;
+  transition: opacity 0.2s;
+  font-family: 'Montserrat', sans-serif;
+  font-weight: 800;
+}
+.add-btn:hover::before,
+.add-btn:hover::after {
+  width: 4px;
+  border-radius: 2px;
+}
+.add-btn:hover .btn-txt {
+  opacity: 1;
+}
+.add-icon::after,
+.add-icon::before {
+  transition: all 0.2s ease-in-out;
+  content: '';
+  position: absolute;
+  height: 20px;
+  width: 2px;
+  top: calc(50% - 10px);
+  /* background: red; */
+  background: green;
+  overflow: hidden;
+}
+.add-icon::before {
+  left: 22px;
+  border-top-left-radius: 2px;
+  border-bottom-left-radius: 2px;
+}
+.add-icon::after {
+  right: 22px;
+  border-top-right-radius: 2px;
+  border-bottom-right-radius: 2px;
+}
+.add-btn:hover .add-icon::before {
+  left: 15px;
+  height: 4px;
+  top: calc(50% - 2px);
+}
+.add-btn:hover .add-icon::after {
+  right: 15px;
+  height: 4px;
+  top: calc(50% - 2px);
+}
+
 .product-pictures {
   height: 10vw;
+  margin-top: 20px;
+  margin-bottom: -20px;
 }
 </style>
